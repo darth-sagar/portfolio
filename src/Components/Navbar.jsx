1f89d922e2dfd084f22ebd095ac86163d821f434
Sagar Saini <work.sagarsaini@gmail.com>
2025-07-04 22:22:17 +0530

    navbar-complete

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -24,11 +24,11 @@ export const Navbar = () => {
 
     }
     const clickOpenMenu = () => {
-        setIsOpen(!isOpen);
+        setIsOpen(prev=>!prev);
     }
     return (
         <>
-            <div className={` fixed flex flex-col items-center justify-center h-[16vh] w-full`}>
+            <div className={`absolute flex flex-col items-center justify-center h-[16vh] w-full`}>
                 <nav className={"w-[92vw] h-[10vh] flex items-center justify-between px-4"}>
                     <div className={`${darkTheme?  "" : "border-black"} bg-white rounded-full w-12 h-12 flex items-center justify-center`}>
                         <img src={logo} alt="Logo" className="w-10 h-10 " />
@@ -38,12 +38,12 @@ export const Navbar = () => {
                         <button>WORK</button>
                         <button>CONTACT</button>
                         <button >
-                            <div  onClick={clickChangeTheme}  className={`${darkTheme ? "bg-white text-black" : "bg-black text-white"} w-12 h-12 flex flex-col items-center justify-center rounded-full text-black `} >
+                            <div  onClick={clickChangeTheme}  className={`${darkTheme ? "bg-white text-black" : "bg-black text-white"} w-12 h-12 flex flex-col items-center justify-center rounded-full text-black z-10`} >
                                 {darkTheme? <LuSunDim id="themebutton" /> :<IoMoonOutline id="themebutton" /> }
                             </div>
                         </button>
                     </div>
-                    <button  onClick={clickOpenMenu} className={"md:hidden text-white button z-20 text-3xl"}>{
+                    <button  onClick={clickOpenMenu} className={`${darkTheme?"text-white":"text-black"} md:hidden  button z-20 text-3xl`}>{
                         isOpen? <RxCross2 />  :<GiHamburgerMenu />
                     }
                     </button>
@@ -53,7 +53,7 @@ export const Navbar = () => {
                 isOpen && (
                     <>
 
-                        <div className={" bg-black/50  md:hidden flex flex-col items-center justify-center space-y-5 w-full h-[100vh] absolute top-0 left-0 z-10"}>
+                        <div className={ `${darkTheme ? "text-white"  : "text-black" } bg-black/30  md:hidden flex flex-col items-center justify-center space-y-5 w-full h-[100vh] absolute top-0 left-0 z-10`}>
                             <button>About</button>
                             <button>Work</button>
                             <button>Contact</button>
