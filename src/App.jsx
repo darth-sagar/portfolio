b040b4c89bda7963de40a4de3f4d13409f426056
Sagar Saini <work.sagarsaini@gmail.com>
2025-07-04 23:55:09 +0530

    hero section started

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import {Navbar} from './Components/Navbar.jsx'
-// import {Hero} from "./Components/Hero.jsx"
+import {Hero} from "./Components/Hero.jsx"
 import {useDarkTheme} from './Context/Context.jsx';
+
 const App = () => {
     const {darkTheme} = useDarkTheme();
     return (
         <div>
             <div className={`w-full h-screen ${darkTheme ? "bg-[#0A0A0A]" : "bg-white"} `}>
                 <Navbar />
+                <Hero/>
             </div>
         </div>
     )
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,26 +1,40 @@
-import React from 'react'
-import {gsap} from 'gsap';
+import {React, useRef} from 'react'
+import gsap from 'gsap';
 import {useGSAP} from '@gsap/react';
+import {ScrollTrigger} from 'gsap/all';
 
-export const Hero = () => {
-    useGSAP(
-        ()=>{
-            gsap.to("#patanahi",{
-                rotate:360,
-                duration: 2,
-                ease: "power1.inOut",
-                x:1500,
+gsap.registerPlugin(ScrollTrigger)
+
+const Hero = () => {
+
+    const scrollref = useRef();
+
+    useGSAP(() => {
+        const sections = gsap.utils.toArray(scrollref.current.children);
+        sections.forEach(section => {
+            gsap.to(section, {
+                scrollTrigger: {
+                    trigger: section,
+                    start: "top 80%",
+                    end: "top 30%",
+                    scrub: true,
+                    toggleActions: "play none none reverse",
+                },
+                y: -100,
+                opacity: 0,
+                duration: 1.5,
             })
-        },[]
-    )
+        })
+    },[]);
+
     return (
-        <div className={"h-full relative"}>
-            <div >
-                <h1>Hi I'm Sagar</h1>
-                <h1>A Developer </h1>
-            </div>
-            <div id ="patanahi"className={'w-20 h-20 bg-white '}>
+        <div >
+            <div className={"w-[300%] rotate-9 absolute top-[15rem] space-y-8 text-[7rem]"} ref={scrollref}>
+                    <h1 id={"first"}>TypeScript / JavaScript / C++ / Python /Redux</h1>
+                    <h1 id={"second"}>Node.js / React.js / G-SAP / javascript / Tailwind CSS</h1>
+                    <h1 id={"first"}>G-SAP / Three.js / CSS / HTML / Framer Motion</h1>
             </div>
         </div>
     )
 }
+export default Hero;
