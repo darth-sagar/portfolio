1618a0338708ac7243695a99e3a38103f8452d1a
Sagar Saini <work.sagarsaini@gmail.com>
2025-07-22 22:36:43 +0530

    landing page setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
-import {Navbar} from './Components/Navbar.jsx'
-import {Hero} from "./Components/Hero.jsx"
-import {useDarkTheme} from './Context/Context.jsx';
+import { Navbar } from './Components/Navbar.jsx'
+import Hero from "./Components/Hero.jsx"
+import { useDarkTheme } from './Context/Context.jsx';
+import LandingPage from './Pages/LandingPage.jsx';
 
 const App = () => {
-    const {darkTheme} = useDarkTheme();
+    const { darkTheme } = useDarkTheme();
     return (
         <div>
+
             <div className={`w-full h-screen ${darkTheme ? "bg-[#0A0A0A]" : "bg-white"} `}>
+                <LandingPage />
                 {/*<Navbar />*/}
-                <Hero/>
+                {/* <Hero /> */}
             </div>
         </div>
     )
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -5,12 +5,32 @@ import {ScrollTrigger} from 'gsap/all';
 
 gsap.registerPlugin(ScrollTrigger)
 
-export const Hero = () => {
+const Hero = () => {
 
+    const scrollref = useRef();
+
+    useGSAP(() => {
+        const sections = gsap.utils.toArray(scrollref.current.children);
+        sections.forEach(section => {
+            gsap.to(section, {
+                scrollTrigger: {
+                    trigger: section,
+                    start: "top 10%",
+                    end: "80% 80%",
+                    scrub: true,
+                    toggleActions: "play none none reverse",
+                    markers: true,
+                },
+                y: -100,
+                opacity: 0,
+                duration: 1.5,
+            })
+        })
+    },[]);
 
     return (
         <div >
-            <div className={"w-[300%] rotate-9 absolute top-[15rem] space-y-8 text-[7rem]"}>
+            <div className={"w-[300%] rotate-9 absolute top-[15rem] space-y-8 text-[7rem]"} ref={scrollref}>
                     <h1 id={"first"}>TypeScript / JavaScript / C++ / Python /Redux</h1>
                     <h1 id={"second"}>Node.js / React.js / G-SAP / javascript / Tailwind CSS</h1>
                     <h1 id={"first"}>G-SAP / Three.js / CSS / HTML / Framer Motion</h1>
@@ -18,3 +38,4 @@ export const Hero = () => {
         </div>
     )
 }
+export default Hero;
