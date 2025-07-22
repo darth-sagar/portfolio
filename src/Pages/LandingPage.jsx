c767262b06e6feda26db52683968514815e992f9
Sagar Saini <work.sagarsaini@gmail.com>
2025-07-23 00:30:28 +0530

    landing page setup

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import {useGSAP} from '@gsap/react';
+import gsap from 'gsap';
 const LandingPage = () => {
+  useGSAP(()=>{
+    gsap.to("#landingPage", {
+        y: -500,
+        opacity: 0,
+        duration: 3,
+        ease: "power1.inOut",
+        repeat:-1,
+    })
+  })
   return (
-    <div className={'w-full h-full flex justify-center items-center absolute'}>
+    <div id={'landingPage'} className={'w-full h-full flex justify-center items-center absolute bg-[#0A0A0A] overflow-hidden'} >
       <div className={'h-full w-1 bg-[#FF4D4D] relative right-[140px] rotate-6'}></div>
       <div className={'h-full w-1 bg-[#FF4D4D] relative right-[40px] rotate-6 md:right-[40px]'}></div>
       <div >
