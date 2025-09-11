96846fc95888e1feb5c11f270c10518c8c8e7eeb
sagar saini <work.sagarsaini@gmail.com>
2025-09-12 00:49:27 +0530

    updated responive design

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -78,10 +78,10 @@ const Hero = () => {
     return (
         <div className={'h-svh overflow-y-hidden overflow-x-hidden md:h-svh relative'}>
             <div className={`flex flex-col justify-center align-middle items-center h-svh`}>
-                <h1 id={'text'} className={`md:text-8xl z-10 ${darkTheme? "" :"text-black"}`}>Crafting Digital Experiences</h1>
-                <h1 id={'text'} className={`md:text-8xl z-10 mb-10 ${darkTheme? "" :"text-black"} `}>Through Code & Design</h1>
-                <h1 id={'text'} className={`md:mt-10 md:text-xl z-10 ${darkTheme? "" :"text-black"} `}>Building immersive web experiences that push</h1>
-                <h1 id={'text'} className={`md:text-xl z-10 ${darkTheme? "" :"text-black"} `}>the boundaries of what's possible</h1>
+                <h1 id={'text'} className={`md:text-8xl text-2xl z-10 ${darkTheme? "" :"text-black"}`}>Crafting Digital Experiences</h1>
+                <h1 id={'text'} className={`md:text-8xl text-2xl z-10 mb-10 ${darkTheme? "" :"text-black"} `}>Through Code & Design</h1>
+                <h1 id={'text'} className={`mt-10 md:text-xl text-xl z-10 ${darkTheme? "" :"text-black"} `}>Building immersive web experiences that push</h1>
+                <h1 id={'text'} className={`md:text-xl text-xl z-10 ${darkTheme? "" :"text-black"} `}>the boundaries of what's possible</h1>
             </div>
             <div id={"hero"} className={"w-[300%] h-1/2 rotate-6 font-light absolute top-1/4 space-y-52 text-4xl md:text-2xl md:text-[8rem] md:space-y-8 md:w-[400%] md:top-[15rem] md:h-full z-0"}>
                     <h1 id={"first"} className={`${darkTheme?"":"text-[#E9E9E9]"} text-[#1B1B1B]`}>TypeScript / JavaScript / C++ / Python / Redux / Vercel</h1>
